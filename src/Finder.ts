89b3218e7a9efebabe7b1f880dbb984224c44afa
FFengIll <fengyouzheng@gmail.com>
2022-05-27 17:32:35 +0800

    feat: new command "find.findInFile" using `workbench.action.findInFiles`.

--- src/Finder.ts
+++ src/Finder.ts
@@ -1,298 +1,312 @@
-"use strict";
+/* eslint-disable @typescript-eslint/naming-convention */
+"use strict"
 
-import * as vscode from 'vscode';
-import * as path from 'path';
-import * as child_process from 'child_process';
+import * as vscode from "vscode"
+import * as path from "path"
+import * as child_process from "child_process"
 
 // Loads custom rule configuration
-function loadCustomConfig() {
+function loadCustomConfig() {}
 
-}
+var colors: any = {}
 
-var colors = {};
-
-function getColorDecoration(color = '#FF0000') {
-    if (1) {
-        var rules = {};
-        var markerType = "background";
-        switch (markerType) {
-            case 'background':
-                rules['backgroundColor'] = color;
-                rules['color'] =/* getColorContrast(color) === */'dark' ? '#111' : '#fff';
-                break;
-            case 'underline':
-                rules['color'] = 'inherit; border-bottom:solid 2px ' + color;
-                break;
-            default:  // outline
-                rules['borderColor'] = color;
-                rules['borderStyle'] = 'solid';
-                rules['borderWidth'] = '2px';
-                break;
-        }
-        colors[color] = vscode.window.createTextEditorDecorationType(rules);
+function getColorDecoration(color = "#FF0000") {
+  if (1) {
+    var rules: any = {}
+    var markerType = "background"
+    switch (markerType) {
+      case "background":
+        rules["backgroundColor"] = color
+        rules["color"] = /* getColorContrast(color) === */ "dark"
+          ? "#111"
+          : "#fff"
+        break
+      case "underline":
+        rules["color"] = "inherit; border-bottom:solid 2px " + color
+        break
+      default: // outline
+        rules["borderColor"] = color
+        rules["borderStyle"] = "solid"
+        rules["borderWidth"] = "2px"
+        break
     }
-    var res = colors[color];
-    return res;
+    colors[color] = vscode.window.createTextEditorDecorationType(rules)
+  }
+  var res: any = colors[color]
+  return res
 }
 
 /**
  * Finder
  */
 export default class Finder {
-    private diagnosticCollection: vscode.DiagnosticCollection;
-    private channel: vscode.OutputChannel;
+  private diagnosticCollection: vscode.DiagnosticCollection
+  private channel: vscode.OutputChannel
 
-    private extensionName: string;
-    private showPosition: vscode.Position;
+  private extensionName: string
+  private showPosition: vscode.Position
 
-    private engine: string;
-    private engine_options: Array<string>;
+  private engine: string
+  private engine_options: Array<string>
 
-    private useDiagnostics: Boolean;
+  private useDiagnostics: Boolean | undefined
 
-    static EXTENSION_NAME = 'find-more';
+  static EXTENSION_NAME = "find-more"
 
-    constructor(context: vscode.ExtensionContext) {
-        this.extensionName = "find-more";
+  constructor(context: vscode.ExtensionContext) {
+    this.extensionName = "find-more"
 
-        // output channel
-        this.channel = vscode.window.createOutputChannel(Finder.EXTENSION_NAME);
+    // output channel
+    this.channel = vscode.window.createOutputChannel(Finder.EXTENSION_NAME)
 
-        // get ripgrep
-        this.engine = vscode.workspace.getConfiguration().get<string>('find.engine');
-        var option_str = vscode.workspace.getConfiguration().get<string>('find.options');
-        this.engine_options = option_str.split(' ');
+    // get ripgrep
+    this.engine =
+      vscode.workspace.getConfiguration().get<string>("find.engine") || ""
+    var option_str: string =
+      vscode.workspace.getConfiguration().get<string>("find.options") || ""
+    this.engine_options = option_str.split(" ")
 
-        // use  diagnostics
-        this.useDiagnostics = vscode.workspace.getConfiguration().get<boolean>('find.diagnostics');
+    // use  diagnostics
+    this.useDiagnostics = vscode.workspace
+      .getConfiguration()
+      .get<boolean>("find.diagnostics")
 
-        // Create DiagnosticCollection
-        this.diagnosticCollection = vscode.languages.createDiagnosticCollection(this.extensionName);
-        context.subscriptions.push(this.diagnosticCollection);
+    // Create DiagnosticCollection
+    this.diagnosticCollection = vscode.languages.createDiagnosticCollection(
+      this.extensionName
+    )
+    context.subscriptions.push(this.diagnosticCollection)
 
-        this.channel = vscode.window.createOutputChannel("find all channel");
+    this.channel = vscode.window.createOutputChannel("find all channel")
 
-        // Load custom rule config
-        loadCustomConfig();
+    // Load custom rule config
+    loadCustomConfig()
 
-        this.showPosition = new vscode.Position(10, 0);
+    this.showPosition = new vscode.Position(10, 0)
+  }
+
+  private get_indexes(text: string, target: string): any {
+    let chs = new Array()
+
+    let maxlen = text.length
+    let len = target.length
+    let cur = 0
+
+    while (cur < maxlen) {
+      var ch = text.indexOf(target, cur)
+      if (ch < 0) {
+        break
+      } else {
+        chs.push(ch)
+        cur = ch + len
+      }
     }
 
-    private get_indexes(text: string, target: string): any {
-        let chs = new Array();
+    return chs
+  }
 
-        let maxlen = text.length;
-        let len = target.length;
-        let cur = 0;
+  public find(target: string) {
+    // get active text editor
+    let editor: any = vscode.window.activeTextEditor
+    let doc = editor.document
+    let uri = doc.uri
+    let lineCount = doc.lineCount
 
-        while (cur < maxlen) {
-            var ch = text.indexOf(target, cur);
-            if (ch < 0) {
-                break;
-            } else {
-                chs.push(ch);
-                cur = ch + len;
-            }
+    // data array
+    var diagnostics: any[] = []
+    var locations: any[] = []
+    var ranges = []
+    var lines = []
+    var filepath = editor.document.uri.fsPath.toString()
+
+    //try if we have rg
+    var options = []
+    options.push(target)
+    options.push(filepath)
+    options = options.concat(this.engine_options)
+
+    this.channel.show()
+
+    var completed = false
+    console.log(this.engine, options.toString())
+    if (this.engine) {
+      var rg = child_process.spawnSync(this.engine, options)
+      if (rg.stdout != null) {
+        completed = true
+        var output = rg.stdout.toString()
+        console.log(output)
+
+        //work both with \r and \n
+        var found = output.split("\n")
+        if (found.length <= 1) {
+          found = output.split("\r")
         }
-
-        return chs;
-    }
-    
-    public find(target: string) {
-        // get active text editor
-        let editor = vscode.window.activeTextEditor;
-        let doc = vscode.window.activeTextEditor.document;
-        let uri = doc.uri
-        let lineCount = doc.lineCount;
-
-        // data array
-        var diagnostics = [];
-        var locations = [];
-        var ranges = [];
-        var lines = [];
-        var filepath = vscode.window.activeTextEditor.document.uri.fsPath.toString();
-
-        //try if we have rg
-        var options = [];
-        options.push(target);
-        options.push(filepath);
-        options = options.concat(this.engine_options);
-
-        this.channel.show();
-
-        var completed = false;
-        console.log(this.engine, options.toString())
-        if (this.engine) {
-            var rg = child_process.spawnSync(this.engine, options);
-            if (rg.stdout != null) {
-                completed = true;
-                var output = rg.stdout.toString();
-                console.log(output);
-
-                //work both with \r and \n
-                var found = output.split('\n');
-                if (found.length <= 1) {
-                    found = output.split('\r');
-                }
-                //process
-                found.forEach(element => {
-                    var pair = element.split(':');
-                    if (pair.length <= 1) {
-                        return;
-                    }
-                    //start from 0
-                    var lineno = parseInt(pair[0]) - 1;
-                    lines.push(lineno);
-                })
-
-            } else {
-                completed = false;
-            }
-        }
-
-        if (!completed) {
-            this.channel.appendLine("no engine or error, use general find instead!");
-            console.log("no engine or error, use general find instead!");
-            //if no engine or error, general find
-            for (var lineno = 0; lineno < lineCount; lineno++) {
-                let text = editor.document.lineAt(lineno).text;
-                let index = text.indexOf(target);
-                if (index >= 0) {
-                    lines.push(lineno);
-                }
-            }
-        }
-
-        lines.forEach(lineno => {
-
-            let text = editor.document.lineAt(lineno).text;
-            let indexes = this.get_indexes(text, target);
-
-            indexes.forEach(index => {
-                //set position, range, uri to show as reference
-                let start = new vscode.Position(lineno, index);
-                let end = new vscode.Position(lineno, index + target.length);
-                let range = new vscode.Range(start, end);
-                let location = new vscode.Location(uri, range);
-                locations.push(location);
-
-                //show in log
-                let message = filepath + "\t" + lineno + "\t" + text;
-                this.channel.appendLine(message);
-                console.log(message);
-
-                //process diagnostic
-                if (this.useDiagnostics) {
-                    var ruleName = "found";//result.ruleName;
-                    var ruleDescription = 'found';//result.ruleDescription;
-
-                    //ruleName + "/" + result.ruleAlias + ": " + ruleDescription;
-
-                    //var range = new vscode.Range(lineno, index, lineno, index + target.length);
-                    ranges.push(range);
-
-                    var diagnostic = new vscode.Diagnostic(range, text, vscode.DiagnosticSeverity.Information);
-
-                    diagnostic.code = 'found';
-                    diagnostics.push(diagnostic);
-                }
-            });
-
-        });
-
-        this.showInReference(uri, locations);
-        if (this.useDiagnostics) {
-            this.showInProblems(uri, diagnostics);
-        }
-    }
-
-    public find2017(target: string) {
-        // get active text editor
-        let editor = vscode.window.activeTextEditor;
-        let doc = vscode.window.activeTextEditor.document;
-        let uri = doc.uri
-        let totalline = doc.lineCount;
-
-        // data array
-        var diagnostics = [];
-        var foundLocations = [];
-        var ranges = [];
-
         //process
-        for (var line = 0; line < totalline; line++) {
-            let text = editor.document.lineAt(line).text;
-            let chs = this.get_indexes(text, target);
+        found.forEach((element) => {
+          var pair = element.split(":")
+          if (pair.length <= 1) {
+            return
+          }
+          //start from 0
+          var lineno = parseInt(pair[0]) - 1
+          lines.push(lineno)
+        })
+      } else {
+        completed = false
+      }
+    }
 
-            chs.forEach(ch => {
-                //set position, range, uri
-                let start = new vscode.Position(line, ch);
-                let end = new vscode.Position(line, ch + target.length);
-                let range = new vscode.Range(start, end);
-                let location = new vscode.Location(uri, range);
-                foundLocations.push(location);
-            });
-
-            chs.forEach(ch => {
-                let desc = uri + "\t" + line + "\t" + text;
-
-                //process diagnostic
-                var ruleName = "found";//result.ruleName;
-                var ruleDescription = 'found';//result.ruleDescription;
-                var message = text;
-                //ruleName + "/" + result.ruleAlias + ": " + ruleDescription;
-
-                var range = new vscode.Range(line, ch, line, ch + target.length);
-                ranges.push(range);
-
-                var diagnostic = new vscode.Diagnostic(range, message, vscode.DiagnosticSeverity.Information);
-
-                diagnostic.code = 'found';
-                diagnostics.push(diagnostic);
-            });
-
+    if (!completed) {
+      this.channel.appendLine("no engine or error, use general find instead!")
+      console.log("no engine or error, use general find instead!")
+      //if no engine or error, general find
+      for (var lineno = 0; lineno < lineCount; lineno++) {
+        let text = editor.document.lineAt(lineno).text
+        let index = text.indexOf(target)
+        if (index >= 0) {
+          lines.push(lineno)
         }
-
-        this.showInProblems(uri, diagnostics);
-        this.showInReference(uri, foundLocations);
+      }
     }
 
-    /**
-     * use executeCommand with editor.action.showReferences to show find results
-     * 
-     * Official Doc:
-     * editor.action.showReferences - Show references at a position in a file
-     * uri The text document in which to show references
-     * position The position at which to show
-     * locations An array of locations.
-     * @param target 
-     */
-    private async showInReference(showUri: vscode.Uri, foundLocations: any[]) {
-        let showPosition = this.showPosition;
-        //use command to show
-        var res = await vscode.commands.executeCommand(
-            'editor.action.showReferences',
-            showUri, showPosition, foundLocations);
-        //console.log(res);
+    lines.forEach((lineno) => {
+      let text = editor.document.lineAt(lineno).text
+      let indexes = this.get_indexes(text, target)
+
+      indexes.forEach((index: number) => {
+        //set position, range, uri to show as reference
+        let start = new vscode.Position(lineno, index)
+        let end = new vscode.Position(lineno, index + target.length)
+        let range = new vscode.Range(start, end)
+        let location = new vscode.Location(uri, range)
+        locations.push(location)
+
+        //show in log
+        let message = filepath + "\t" + lineno + "\t" + text
+        this.channel.appendLine(message)
+        console.log(message)
+
+        //process diagnostic
+        if (this.useDiagnostics) {
+          var ruleName = "found" //result.ruleName;
+          var ruleDescription = "found" //result.ruleDescription;
+
+          //ruleName + "/" + result.ruleAlias + ": " + ruleDescription;
+
+          //var range = new vscode.Range(lineno, index, lineno, index + target.length);
+          ranges.push(range)
+
+          var diagnostic = new vscode.Diagnostic(
+            range,
+            text,
+            vscode.DiagnosticSeverity.Information
+          )
+
+          diagnostic.code = "found"
+          diagnostics.push(diagnostic)
+        }
+      })
+    })
+
+    this.showInReference(uri, locations)
+    if (this.useDiagnostics) {
+      this.showInProblems(uri, diagnostics)
+    }
+  }
+
+  public find2017(target: string) {
+    // get active text editor
+    let editor: any = vscode.window.activeTextEditor
+    let doc = editor.document
+    let uri = doc.uri
+    let totalline = doc.lineCount
+
+    // data array
+    var diagnostics: any[] = []
+    var foundLocations: any[] = []
+    var ranges = []
+
+    //process
+    for (var line = 0; line < totalline; line++) {
+      let text = editor.document.lineAt(line).text
+      let chs = this.get_indexes(text, target)
+
+      chs.forEach((ch: number) => {
+        //set position, range, uri
+        let start = new vscode.Position(line, ch)
+        let end = new vscode.Position(line, ch + target.length)
+        let range = new vscode.Range(start, end)
+        let location = new vscode.Location(uri, range)
+        foundLocations.push(location)
+      })
+
+      chs.forEach((ch: number) => {
+        let desc = uri + "\t" + line + "\t" + text
+
+        //process diagnostic
+        var ruleName = "found" //result.ruleName;
+        var ruleDescription = "found" //result.ruleDescription;
+        var message = text
+        //ruleName + "/" + result.ruleAlias + ": " + ruleDescription;
+
+        var range = new vscode.Range(line, ch, line, ch + target.length)
+        ranges.push(range)
+
+        var diagnostic = new vscode.Diagnostic(
+          range,
+          message,
+          vscode.DiagnosticSeverity.Information
+        )
+
+        diagnostic.code = "found"
+        diagnostics.push(diagnostic)
+      })
     }
 
-    /**
-     * use vscode.Diagnostic to show find results in PROBLEMS panel
-     * @param target 
-     */
-    private showInProblems(uri: vscode.Uri, diagnostics: any[]) {
-        //clean
-        this.clear();
+    this.showInProblems(uri, diagnostics)
+    this.showInReference(uri, foundLocations)
+  }
 
-        var color = getColorDecoration();
+  /**
+   * use executeCommand with editor.action.showReferences to show find results
+   *
+   * Official Doc:
+   * editor.action.showReferences - Show references at a position in a file
+   * uri The text document in which to show references
+   * position The position at which to show
+   * locations An array of locations.
+   * @param target
+   */
+  private async showInReference(showUri: vscode.Uri, foundLocations: any[]) {
+    let showPosition = this.showPosition
+    //use command to show
+    var res = await vscode.commands.executeCommand(
+      "editor.action.showReferences",
+      showUri,
+      showPosition,
+      foundLocations
+    )
+    //console.log(res);
+  }
 
-        // Publish
-        //editor.setDecorations(getColorDecoration(), ranges);
-        //this.channel.show();
-        this.diagnosticCollection.set(uri, diagnostics);
-    }
+  /**
+   * use vscode.Diagnostic to show find results in PROBLEMS panel
+   * @param target
+   */
+  private showInProblems(uri: vscode.Uri, diagnostics: any[]) {
+    //clean
+    this.clear()
 
-    public clear() {
-        this.channel.clear();
-        this.diagnosticCollection.clear();
-    }
+    var color = getColorDecoration()
+
+    // Publish
+    //editor.setDecorations(getColorDecoration(), ranges);
+    //this.channel.show();
+    this.diagnosticCollection.set(uri, diagnostics)
+  }
+
+  public clear() {
+    this.channel.clear()
+    this.diagnosticCollection.clear()
+  }
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,43 +1,68 @@
-"use strict";
+"use strict"
 
-import * as vscode from 'vscode';
-import * as path from 'path';
-import Finder from './Finder';
+import * as vscode from "vscode"
+import Finder from "./Finder"
+import { FindTextInFilesOptions } from "vscode"
+import { openStdin } from "process"
 
+export interface IFindInFilesArgs {
+  query?: string
+  replace?: string
+  preserveCase?: boolean
+  triggerSearch?: boolean
+  filesToInclude?: string
+  filesToExclude?: string
+  isRegex?: boolean
+  isCaseSensitive?: boolean
+  matchWholeWord?: boolean
+  useExcludeSettingsAndIgnoreFiles?: boolean
+  onlyOpenEditors?: boolean
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  //console.log('Congratulations, your extension "find-more" is now active!');
 
-    //console.log('Congratulations, your extension "find-more" is now active!');
+  // let finder = new Finder(context)
 
-    let finder = new Finder(context);
+  //core contributions
+  context.subscriptions.push(
+    vscode.commands.registerCommand("find.findInFile", async function () {
+      var editor = vscode.window.activeTextEditor
+      if (editor == null) {
+        return
+      }
+      var target = editor.document.getText(editor.selection)
+      var path = editor.document.uri.fsPath
+      vscode.workspace.getWorkspaceFolder
 
-    //core contributions
-    let disposable = null;
-    disposable = vscode.commands.registerCommand('find.findAll', () => {
-
-        var editor = vscode.window.activeTextEditor;
-        var target = editor.document.getText(editor.selection);
-
-        if (target.length > 0) {
-            finder.find(target);
-        } else {
-            vscode.window.showInputBox({ prompt: 'target' })
-                .then(target => {
-                    finder.find(target);
-                });
+      var include: string[] = []
+      vscode.workspace.workspaceFolders?.map((folder) => {
+        var folderPath = folder.uri.path
+        if (path.startsWith(folderPath)) {
+          var splitor = path.charAt(folderPath.length)
+          if (splitor == "\\" || splitor == "/") {
+            include.push(path.substring(folderPath.length + 1))
+          }
         }
-    });
-    context.subscriptions.push(disposable);
+      })
 
-    disposable = vscode.commands.registerCommand('find.clear', () => {
-        finder.clear();
-    });
-    context.subscriptions.push(disposable);
+      console.info(path)
+      console.info(include)
+
+      var args: IFindInFilesArgs = {
+        query: target,
+        filesToInclude: include.join(","),
+        triggerSearch: true,
+      }
+
+      vscode.commands.executeCommand("workbench.action.findInFiles", {
+        args,
+      })
+    })
+  )
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {
-
-}
+export function deactivate() {}
